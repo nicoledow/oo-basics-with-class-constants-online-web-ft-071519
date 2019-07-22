077dcf57a968cfd98b708aec9cbd6564d71da92f
Nicole Dow <nikki.dow71@gmail.com>
2019-07-22 18:41:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,18 +6,14 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-  
-  def brand=(brand)
-    @brand = brand
-    if !BRANDS.include?(brand)
-      BRANDS << brand
-    end
   end
 
 end
